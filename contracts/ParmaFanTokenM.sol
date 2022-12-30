bf5b74340bbfadffff62db1c8f9d73561621cdbf
Matteo Celani <matteocelani4@gmail.com>
2022-12-30 16:12:21 +0100

    Update ParmaFanTokenM.sol

--- contracts/ParmaFanTokenM.sol
+++ contracts/ParmaFanTokenM.sol
@@ -1,8 +1,33 @@
 // SPDX-License-Identifier: ISC
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "/contracts/ParmaFanToken.sol";
 
 pragma solidity ^0.8.4;
 
-contract ParmaFanTokenM is ERC20, Ownable {}
+contract ParmaFanTokenM is Context, Ownable {
+    using SafeMath for uint256;
+
+    mapping(address => bool) private _blacklistAddress;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    uint8 private _decimals;
+    string private _name;
+    string private _symbol;
+
+    address private _ParmaFanTokenAddress;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 totalSupply_, address parmaFanTokenAddress_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        _totalSupply = totalSupply_;
+        _ParmaFanTokenAddress = parmaFanTokenAddress_;
+    }
+    
+}
