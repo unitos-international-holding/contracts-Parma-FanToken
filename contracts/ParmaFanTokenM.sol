e824294213abc7e0e943dea18023d5628183da95
Matteo Celani <matteocelani4@gmail.com>
2022-12-30 15:43:16 +0100

    Create ParmaFanTokenM.sol

---
+++ contracts/ParmaFanTokenM.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: ISC
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.4;
+
+contract ParmaFanTokenM is ERC20, Ownable {}
